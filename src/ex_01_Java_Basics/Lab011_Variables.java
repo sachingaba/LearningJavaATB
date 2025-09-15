20afa1642ce664effab6f7bfe9aec3ae4f4a2f41
sachin <iamsachingaba@gmail.com>
2025-09-15 18:44:20 +0530

    java class 13th Sep

---
+++ src/ex_01_Java_Basics/Lab011_Variables.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab011_Variables {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 11;
+        int z = x + y;
+        byte age = 45;  // byte data type , age - variable name/identifier , 45 is value or literal
+        System.out.println(z);
+        System.out.println("age is " + age);
+
+    }
+}
