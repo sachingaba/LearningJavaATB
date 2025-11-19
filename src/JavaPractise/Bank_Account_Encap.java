54c6d21e64968a02af794cb1f86b4271ca21d737
sachin <iamsachingaba@gmail.com>
2025-11-19 19:45:06 +0530

    java class 19 Nov task

---
+++ src/JavaPractise/Bank_Account_Encap.java
@@ -0,0 +1,64 @@
+package JavaPractise;
+
+import java.util.Scanner;
+
+public class Bank_Account_Encap {
+    public static void main(String[] args) {
+        BankAct b1 = new BankAct(1000);
+      ///  System.out.println("Initial balance: "+ b1.getBal());
+        Scanner scan = new Scanner(System.in);
+
+        int deposit = deposit(scan,"Enter deposit Value");
+        b1.setBal(b1.getBal()+deposit);
+        System.out.println("Your updated balance after deposit: "+ b1.getBal());
+        int with = withdraw(scan,"Enter withdrawal amount");
+
+        while (with>b1.getBal()){
+            System.out.println("Insufficient amount . Enter lower ");
+            with = scan.nextInt();
+        }
+            b1.setBal(b1.getBal()-with);
+
+
+        System.out.println("YOur balance after withdrwal is : " + b1.getBal());
+
+
+    }
+
+    static int deposit(Scanner scan, String prompt){
+        System.out.println(prompt);
+        if(!scan.hasNextInt() || scan.nextInt()<0){
+            System.out.println("Enter proper values");
+        }
+        return scan.nextInt();
+    }
+    static int withdraw(Scanner scan, String prompt){
+        System.out.println(prompt);
+        if(!scan.hasNextInt()){
+            System.out.println("Enter proper values");
+        }
+        return scan.nextInt();
+    }
+}
+
+class BankAct{
+    private double bal;
+    public double deposit;
+    public double withdrawal;
+
+    public double getDeposit() {
+        return deposit;
+    }
+
+    public BankAct(double bal) {
+        this.bal = bal;
+    }
+
+    public double getBal() {
+        return bal;
+    }
+
+    public void setBal(double bal) {
+        this.bal = bal;
+    }
+}
