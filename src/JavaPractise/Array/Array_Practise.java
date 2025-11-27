9be8ce604fcdd8f5817b2632ae762c74e638942b
sachin <iamsachingaba@gmail.com>
2025-11-27 18:16:57 +0530

    java class 25 Nov task

---
+++ src/JavaPractise/Array/Array_Practise.java
@@ -0,0 +1,12 @@
+package JavaPractise.Array;
+
+public class Array_Practise {
+    public static void main(String[] args) {
+        String[] sach = {"sa","sd","fg","fg","hg","g","h"};
+
+        for (int i = 0; i < sach.length; i++) {
+            System.out.print(" "+ sach[i]);
+
+        }
+    }
+}
