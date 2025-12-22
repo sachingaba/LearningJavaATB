7fc66b6ac15fa84cda432f731af10ef6c4b23243
sachin <iamsachingaba@gmail.com>
2025-12-22 22:41:02 +0530

    java class 22nd Dec task

--- src/JavaPractise/Array/Array_practise_01.java
+++ src/JavaPractise/Array/Array_practise_01.java
@@ -4,10 +4,11 @@ public class Array_practise_01 {
     public static void main(String[] args) {
 
 
-    int[] arr = {32,900,425,56,87,89,90,6574,23,905,12,430,6574,700};
+    int[] arr = {32,900,425,56,87,89,90,6574,23,905,12,430,2,6574,700};
     int max = arr[0];
     int min = arr[0];
     int max2 = arr[0];
+    int min2 = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
             if(arr[i]>max){
@@ -31,9 +32,16 @@ public class Array_practise_01 {
                 min = arr[i];
             }
         }
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]<min2 && arr[i]>min){
+                min2 = arr[i];
+            }
+        }
+
         System.out.println(max);
         System.out.println(min);
         System.out.println(arr.length);
         System.out.println(max2);
+        System.out.println(min2);
     }
 }
