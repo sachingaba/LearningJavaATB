073611e344590295d393fae06931ad363bae6a80
sachin <iamsachingaba@gmail.com>
2026-01-02 20:52:47 +0530

    java class 31st Dec task

--- src/ex_30_Collection_Framework/CF_01_LIST/Lab173_List.java
+++ src/ex_30_Collection_Framework/CF_01_LIST/Lab173_List.java
@@ -5,24 +5,24 @@ import java.util.List;
 
 public class Lab173_List {
     public static void main(String[] args) {
-        List fruits = List.of("orange","banana","apple","strawberry","papaya","WaterMelon",5,6.5,"orange");
+        List<Object> fruits = List.of("orange","banana","apple","strawberry","papaya","WaterMelon",5,6.5,"orange");
         System.out.println(fruits);
 
 
 
-        List arrayList = new ArrayList();
+        List<Object> arrayList = new ArrayList<>();
         arrayList.add("sachin");
         arrayList.add("sachin");
         arrayList.add("gaba");
         arrayList.add(true);
         arrayList.add(4.5);
         arrayList.add(123);
-        System.out.println(arrayList.size());
+        System.out.println("SIze: " + arrayList.size());
         System.out.println(arrayList);
         System.out.println(arrayList.isEmpty());
         System.out.println(arrayList.contains(1));
         System.out.println(arrayList.contains(true));
-        System.out.println(arrayList.indexOf("3"));
+        System.out.println("Index of 4.5 : "  + arrayList.indexOf(4.5));
 
         ArrayList a1 = new ArrayList<>();
         a1.add("hello");
@@ -31,9 +31,10 @@ public class Lab173_List {
         System.out.println(a1.contains(4));
         System.out.println(a1.contains("lo"));
 
-        List arraymain = new ArrayList();
-        arraymain.add(a1);
-        arraymain.add(arrayList);
-        System.out.println(arraymain);
+        List arrayMain = new ArrayList();
+        arrayMain.add(a1);
+        arrayMain.add(arrayList);
+        arrayMain.add(fruits);
+        System.out.println(arrayMain);
     }
 }
