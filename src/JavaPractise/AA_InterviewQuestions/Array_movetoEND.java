19d2aac2ebc65e76d3148ecdf5de1d60c8f2efe8
sachin <iamsachingaba@gmail.com>
2026-02-05 17:51:54 +0530

    java class 5th Feb task

---
+++ src/JavaPractise/AA_InterviewQuestions/Array_movetoEND.java
@@ -0,0 +1,26 @@
+package JavaPractise.AA_InterviewQuestions;
+
+import java.util.Arrays;
+
+public class Array_movetoEND {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,1,0,3,1,0};
+
+        int p = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]!=1){
+                arr[p]=arr[i];
+                p++;
+            }
+
+        }
+        while(p < arr.length){
+            arr[p]=1;
+            p++;
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
