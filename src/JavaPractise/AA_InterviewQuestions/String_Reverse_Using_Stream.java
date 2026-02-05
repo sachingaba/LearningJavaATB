300c8896f0f21712d0571c26a3953d16abc32eda
sachin <iamsachingaba@gmail.com>
2026-02-05 16:31:15 +0530

    java class 5th Feb task

---
+++ src/JavaPractise/AA_InterviewQuestions/String_Reverse_Using_Stream.java
@@ -0,0 +1,12 @@
+package JavaPractise.AA_InterviewQuestions;
+
+import java.util.stream.Stream;
+
+public class String_Reverse_Using_Stream {
+    public static void main(String[] args) {
+        String str = "Hello world";
+
+       String rev =  Stream.of(str.split(" ")).reduce("",(a,b)->b+a);
+        System.out.println(rev);
+    }
+}
