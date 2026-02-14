0e9fc454b33793130738c56a324d1a50b20b3d50
sachin <iamsachingaba@gmail.com>
2026-02-14 18:54:53 +0530

    java class 14th Feb task

--- src/JavaPractise/P01_First_Duplicate_In_Array.java
+++ src/JavaPractise/P01_First_Duplicate_In_Array.java
@@ -4,19 +4,24 @@ public class P01_First_Duplicate_In_Array {
 
         public static void main(String[] args) {
 
-            int[] array = {22,33,66,55,80,23,44,44};
+            int[] array = {22,33,66,55,80,23,44,44,80};
 
-
-            int max1= array[array.length-1];
-            System.out.println("2nd Max " + max1);
 
             int max = array[0];
+            int max2nd = 0;
             for (int i=0;i<=array.length-1;i++){
-                if(array[i]>max){
+                if(array[i]>=max){
+                    int temp = max;
                     max=array[i];
+                    if(temp<=max){
+                        max2nd=temp;
+                    }
+
                 }
+
             }
             System.out.println("Max " + max);
+            System.out.println("Max 2nd " + max2nd);
 
             int[] nums = {1, 0, 3, 2, 4, 5,1 , 1 , 0};
 
