c526ba51d3e1c819df94f563346acfc9054c3d53
SachinGaba <iamsachingaba@gmail.com>
2026-04-06 18:46:32 +0530

    6th Apr new Programs

--- src/JavaPractise/AA_InterviewQuestions/Array_Duplicate_For_Loop.java
+++ src/JavaPractise/AA_InterviewQuestions/Array_Duplicate_For_Loop.java
@@ -10,7 +10,7 @@ public class Array_Duplicate_For_Loop {
         int[] narr = new int[arr.length];
 
         for(int i=0;i<arr.length;i++){
-            if(arr[i]==100){
+            if(arr[i]==-1){
                 continue;
             }
 
