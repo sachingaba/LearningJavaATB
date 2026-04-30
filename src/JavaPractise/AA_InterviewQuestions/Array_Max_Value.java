98eecd3d011e473279d343cbbc9f00b424ca23b8
SachinGaba <iamsachingaba@gmail.com>
2026-04-30 12:29:58 +0530

    30th Apr new Programs

--- src/JavaPractise/AA_InterviewQuestions/Array_Max_Value.java
+++ src/JavaPractise/AA_InterviewQuestions/Array_Max_Value.java
@@ -1,10 +1,13 @@
 package JavaPractise.AA_InterviewQuestions;
 
+import java.util.Arrays;
+
 public class Array_Max_Value {
     public static void main(String[] args) {
 
         int[] arr = {2,45,7,78,345,323,68,9,343,2};
-        int max=0;
+        int n = 3;
+        int max= arr[0];
         for (int j : arr) {
 
             if (j > max) {
@@ -12,5 +15,9 @@ public class Array_Max_Value {
             }
         }
         System.out.println(max);
+
+        Arrays.sort(arr);
+        int nthMax = arr[arr.length-n];
+        System.out.println(nthMax);
     }
 }
