9846f6bf3f2dec18a1f74cdf39a0319771ec6396
SachinGaba <iamsachingaba@gmail.com>
2026-05-17 19:47:38 +0530

    17th May new Programs

--- src/JavaPractise/AA_InterviewQuestions/LongestSubstringWithSameChar.java
+++ src/JavaPractise/AA_InterviewQuestions/LongestSubstringWithSameChar.java
@@ -3,7 +3,7 @@ package JavaPractise.AA_InterviewQuestions;
 public class LongestSubstringWithSameChar {
     public static void main(String[] args) {
 
-        String s1 = "aaaaaaaaaaabcbccccccccbbcbbcsbbbbbabaaaaaaaaaabaa";
+        String s1 = "aaaaaaaaaaabcbccccccccbbcbbcsbbbbbabaaaaaaaaaaaaabaa";
 
         int max =0;
         String sub="";
@@ -12,8 +12,8 @@ public class LongestSubstringWithSameChar {
         for (int i = 0; i < ch.length; i++) {
 
             StringBuilder sb = new StringBuilder();
-            sb.append(ch[i]);
-            for (int j = i+1; j < ch.length; j++) {
+           // sb.append(ch[i]);
+            for (int j = i; j < ch.length; j++) {
                 if(ch[i]==ch[j]){
                     sb.append(ch[j]);
 
