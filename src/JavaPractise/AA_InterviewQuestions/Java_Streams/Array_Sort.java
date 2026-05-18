de49b36a3b6d6b97ab6a383e12bdfb6785c3d9de
SachinGaba <iamsachingaba@gmail.com>
2026-05-18 17:48:18 +0530

    18th May new Programs

--- src/JavaPractise/AA_InterviewQuestions/Java_Streams/Array_Sort.java
+++ src/JavaPractise/AA_InterviewQuestions/Java_Streams/Array_Sort.java
@@ -6,7 +6,8 @@ public class Array_Sort {
     public static void main(String[] args) {
         int[] arr = {1,4,56,89,4,567,3,23,87,8,33};
 
-        int[] array = Arrays.stream(arr).sorted().toArray();
+        int[] array = Arrays.stream(arr)
+                .sorted().toArray();
 
 
         System.out.println(Arrays.toString(arr));
