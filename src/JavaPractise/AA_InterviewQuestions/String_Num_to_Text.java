40f209ecf311d88ba59eb93082a2439eb4d4cc95
SachinGaba <iamsachingaba@gmail.com>
2026-06-11 15:50:06 +0530

    11th June new Programs

--- src/JavaPractise/AA_InterviewQuestions/String_Num_to_Text.java
+++ src/JavaPractise/AA_InterviewQuestions/String_Num_to_Text.java
@@ -8,14 +8,12 @@ public class String_Num_to_Text {
 
         int input = 58697;
         int len = String.valueOf(input).length();
-        //   System.out.println(len);
+
 
         char[] ch = String.valueOf(input).toCharArray();
         System.out.println(Arrays.toString(ch));
         for (int i = 0; i < ch.length; i++) {
             int pow = (int) (Math.pow(10, len - 1));
-    //        System.out.println(pow);
-     //       System.out.println(ch[i]);
             int value = Integer.parseInt(String.valueOf(ch[i])) * pow;
 
             System.out.print(value + " ");
