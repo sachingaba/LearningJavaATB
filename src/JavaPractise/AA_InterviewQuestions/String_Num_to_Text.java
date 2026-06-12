eca9cad8213b114f29ff85c3cf53e2a2d156c71e
SachinGaba <iamsachingaba@gmail.com>
2026-06-12 17:12:06 +0530

    new Programs

--- src/JavaPractise/AA_InterviewQuestions/String_Num_to_Text.java
+++ src/JavaPractise/AA_InterviewQuestions/String_Num_to_Text.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class String_Num_to_Text {
     public static void main(String[] args) {
-        System.out.println("Start small. Ship something.");
+
 
         int input = 58697;
         int len = String.valueOf(input).length();
