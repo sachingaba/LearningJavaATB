bdc3ea68848e07d066fdc08bf6ca5a5d0ec01c50
SachinGaba <iamsachingaba@gmail.com>
2026-06-26 13:40:06 +0530

    new Programs

--- src/JavaPractise/ReverseString_Two_Pointer.java
+++ src/JavaPractise/ReverseString_Two_Pointer.java
@@ -9,20 +9,17 @@ public class ReverseString_Two_Pointer {
         int r = text.length()-1;
         char[] ch = text.toCharArray();
         while(l<r){
-
             if(Character.isLetter(ch[l]) && Character.isLetter(ch[r])){
                 char temp = ch[l];
                 ch[l] = ch[r];
                 ch[r]= temp;
                 l++;
                 r--;
-
             }else if(!Character.isLetter(ch[l])){
                 l++;
             }else{
                 r--;
             }
-
         }
         System.out.println(new String(ch));
     }
