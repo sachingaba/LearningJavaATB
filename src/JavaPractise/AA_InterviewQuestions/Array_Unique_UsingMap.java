60e8a0ee684b4a86204247aaefa31ee20294988a
SachinGaba <iamsachingaba@gmail.com>
2026-09-06 17:48:04 +0530

    new Programs

--- src/JavaPractise/AA_InterviewQuestions/Array_Unique_UsingMap.java
+++ src/JavaPractise/AA_InterviewQuestions/Array_Unique_UsingMap.java
@@ -29,8 +29,6 @@ public class Array_Unique_UsingMap {
         }
         System.out.println(m1);
 
-
-
         for (Map.Entry<Integer,Integer> entry: m1.entrySet()){
             if(entry.getValue()==1){
                 System.out.println(entry.getKey());
